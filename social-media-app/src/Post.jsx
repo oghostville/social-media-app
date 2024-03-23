f10ac22dc150e0b6def578c00680a6745246bc64
Elian Lopez <elianlopez@Elians-MacBook-Pro.local>
2024-03-22 21:58:04 -0400

    Created Post component

---
+++ social-media-app/src/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Post from './components/Post';
+
+function App(){ 
+    return(
+      <div>
+        <h1>Fakebook!</h1>
+        
+          {/* CreatePostForm */}
+          
+          {/* Feed */}
+          
+          <Post /> 
+      
+      </div>
+    )
+  }
+  export default App
