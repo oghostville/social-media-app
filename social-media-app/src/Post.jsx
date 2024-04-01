4a05d60eefd627597279569a257980fc78f6cfd7
oghostville <151267555+oghostville@users.noreply.github.com>
2024-04-01 19:53:23 -0400

    Update Post.jsx
    Updated post for pt2 of HW

--- social-media-app/src/Post.jsx
+++ social-media-app/src/Post.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import Comment from './Comment';
-function Post(props){
+
+function Post({ title, content }) { // Destructure title and content from props
   const [likes, setLikes] = useState(0);
-  function handleLike(){
+  
+  function handleLike() {
     setLikes(likes + 1);
   }
-  return(
+  
+  return (
     <div>
-      <h3>{props.content}</h3>
+      <h2>{title}</h2> {/* Display the title */}
+      <h4>{content}</h4> {/* Display the content */}
       <p>Likes: {likes}</p>
       <button onClick={handleLike}>Like</button>
       <p>Comments:</p>
       <Comment content="This is a test comment!" />
-      <Comment content="This is another test comment!" />
-      <Comment content="This is yet another test comment!" />
     </div>
-  )
+  );
 }
-export default Post
+
+export default Post;
