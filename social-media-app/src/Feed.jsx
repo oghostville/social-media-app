f1839f1691ca7f85cc733e03f56b96de5e5f6ab5
oghostville <151267555+oghostville@users.noreply.github.com>
2024-04-01 19:59:03 -0400

    Create Feed.jsx

---
+++ social-media-app/src/Feed.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post";
+
+function Feed({posts}){
+
+
+    return (
+        <div>
+          {posts.map((post, index) => (
+          <Post key={index} title={post.title} content={post.content} />
+          ))}
+        </div>
+      );
+    }
+
+
+
+export default Feed;
