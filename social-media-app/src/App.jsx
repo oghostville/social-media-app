58ee666bc39f36437ec96254893bb38d43ef8da3
oghostville <151267555+oghostville@users.noreply.github.com>
2024-04-01 19:54:51 -0400

    Update App.jsx
    updated app for pt2 hw

--- social-media-app/src/App.jsx
+++ social-media-app/src/App.jsx
@@ -1,27 +1,33 @@
+
+
 import { useState } from 'react'
+import CreatePostForm from './CreatePostForm'
+import Feed from './Feed';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 import React from 'react';
-import Post from './Post';
 
 function App(){ 
+  const [posts, setPosts] = useState([]); // State to store posts
+
+  // Function to add a new post to the posts state
+  const addPost = (newPost) => {
+    setPosts([newPost, ...posts]); // Add new post at the beginning of the posts array
+  };
+
   return(
     <div>
       <h1>Fakebook!</h1>
       
-        {/* CreatePostForm */}
-        
-        {/* Feed */}
-        
-        {/* Post*/}
-        <Post content="This is a test post!" />
-        <Post content="This is another test post!" />
+      <CreatePostForm addPost={addPost} />
+      <Feed posts={posts} /> 
+      
+      
     
     </div>
   )
 }
-
-
 export default App
